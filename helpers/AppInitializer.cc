d77dc80ae9d6a25d04f5636a670e5d9a80e8a9a7
ecrisost <ecrisost@sfu.ca>
2020-03-06 17:17:53 -0800

    Add files via upload
    add .cc

---
+++ helpers/AppInitializer.cc
@@ -0,0 +1,35 @@
+/*
+ * AppInitializer.cc
+ *
+ *  Created on: Mar 3, 2020
+ *      Author: Elaine Crisostomo
+ *      This initializes all of our fun stuff
+ *      Like buttons and what not
+ */
+
+#include "../helpers/AppInitializer.h"
+
+//Flushes the cache
+void FlushCache(){
+	Xil_DCacheDisable();
+
+	Xil_DCacheInvalidate();
+	Xil_DCacheFlush();
+	Xil_ICacheInvalidate();
+
+}
+
+//Initializes the buttons
+//Taken from: https://reference.digilentinc.com/learn/programmable-logic/tutorials/zedboard-getting-started-with-zynq/start
+void InitializeButtons(XGpio* input){
+	XGpio_Initialize(input, XPAR_GPIO_0_DEVICE_ID);
+	XGpio_SetDataDirection(input, XPAR_GPIO_0_DEVICE_ID, 0xF); //set first channel tristate buffer to input
+
+}
+
+//read the buttons
+int ReadButtons(XGpio* input){
+	int button_data = XGpio_DiscreteRead(input, 1);
+
+	return button_data;
+}
